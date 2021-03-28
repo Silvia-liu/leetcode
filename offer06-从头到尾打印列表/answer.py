7d8e453ae95f8321bd929847d76b9ee061bddd55
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-03-28 17:07:32 +0800

    Create answer.py

---
+++ offer06-从头到尾打印列表/answer.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reversePrint(self, head: List) -> List[int]:
+        stack = []
+        while head:
+            stack.append(head.val)  # 读取head的值
+            head = head.next  # head指针向后一位
+        return stack[::-1]  # list倒序排列
