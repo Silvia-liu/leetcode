47d7df77cb231d899088b95f9fcd8bf42fe311d2
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-05-17 14:07:26 +0800

    Update answer.py

--- offer-59-sliding-window-maximum/answer.py
+++ offer-59-sliding-window-maximum/answer.py
@@ -1,1 +1,22 @@
 
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        if not nums or k == 0: return []
+        deque = collections.deque()
+        # 未形成窗口
+        for i in range(k):
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+        res = [deque[0]]
+        # 形成窗口后
+        for i in range(k, len(nums)):
+            if deque[0] == nums[i - k]:
+                deque.popleft()
+            while deque and deque[-1] < nums[i]:
+                deque.pop()
+            deque.append(nums[i])
+            res.append(deque[0])
+        return res
+        # 对比offer30，本题难点： 如何在每次窗口滑动后，将 “获取窗口内最大值” 的时间复杂度从 O(k)O(k) 降低至 O(1)O(1)
+        # 不同点在于 “出栈操作” 删除的是 “列表尾部元素” ，而 “窗口滑动” 删除的是 “列表首部元素” 。窗口对应的数据结构为 双端队列 ，本题使用 单调队列 即可解决以上问题。
