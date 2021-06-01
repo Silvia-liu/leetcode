895835712f1fca1a6ed02b9c7e5d87074a793075
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-01 09:40:48 +0800

    Update answer.py

--- offer-15-number-of-1-bits/answer.py
+++ offer-15-number-of-1-bits/answer.py
@@ -1,11 +1,16 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # 方法一
+        # 方法一 最常用的方法
         res = 0
         while n:
             res += n & 1
-            n >>= 1
+            n >>= 1  # 逐位左移
         return res
-        # 方法二
-        
-        # 方法三
+    # 方法二  巧用n&(n-1)  &=是按位处理
+#     def hammingWeight(self, n: int) -> int:
+#     res = 0
+#     while n:
+#         res += 1
+#         n &= n - 1
+#     return res
+     
