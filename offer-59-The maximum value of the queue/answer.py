2862be48154cb14dd8ba3e4de27c5bfa574c15e7
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-02 20:48:11 +0800

    Update answer.py

--- offer-59-The maximum value of the queue/answer.py
+++ offer-59-The maximum value of the queue/answer.py
@@ -1,23 +1,23 @@
 import queue
 class MaxQueue:
-
+       # 因题干要求时间复杂度，所以用空间换时间，
     def __init__(self):
         self.queue = queue.Queue()
         self.deque = queue.deque()
 
     def max_value(self) -> int:
-        return self.deque[0] if self.deque else -1
+        return self.deque[0] if self.deque else -1  # 队列为空返回-1
 
     def push_back(self, value: int) -> None:
-        self.queue.put(value)
+        self.queue.put(value)  # value的加入
         while self.deque and self.deque[-1] < value:
-            self.deque.pop()
+            self.deque.pop()  # value的加入让deque不再严格单调递减，所以删除最后一个，让value加入deque尾部
         self.deque.append(value)
 
     def pop_front(self) -> int:
-        if self.queue.empty(): return -1
-        val = self.queue.get()
-        if val == self.deque[0]:
+        if self.queue.empty(): return -1  # 队列为空返回-1
+        val = self.queue.get()  # 拿到queue的最头部的值
+        if val == self.deque[0]:  # 如果这个值，是当前的最大值，所以，辅助队列deque也要删除左侧头部最大值、保证deque的头部永远是最大值
             self.deque.popleft()
         return val
 
