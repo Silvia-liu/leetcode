2ddfe9395d555e1802ac3db074d320ebc919e617
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-03 10:21:53 +0800

    Update answer.py

--- offer-10-climbing-stairs/answer.py
+++ offer-10-climbing-stairs/answer.py
@@ -4,3 +4,13 @@ class Solution:
         for _ in range(n):
             a, b = b, (a + b) % 1000000007
         return a
+    
+ # 方法二
+# 不考虑大数越界问题
+class Solution:
+    def numWays(self, n: int) -> int:
+        a, b = 1, 1
+        for _ in range(n):
+            a, b = b, a + b
+        return a % 1000000007
+# 本题解法和斐波那契数列一样，只是初始值a， b不同而已
