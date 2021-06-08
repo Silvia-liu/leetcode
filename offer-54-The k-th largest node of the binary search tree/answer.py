85def2ab86a236b30d57c07507ccbbb55739bd13
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-08 20:20:38 +0800

    Update answer.py

--- offer-54-The k-th largest node of the binary search tree/answer.py
+++ offer-54-The k-th largest node of the binary search tree/answer.py
@@ -1,13 +1,16 @@
 class Solution:
     def kthLargest(self, root: TreeNode, k: int) -> int:
         def dfs(root):
-            if not root: return
-            dfs(root.right)
-            if self.k == 0: return
-            self.k -= 1
-            if self.k == 0: self.res = root.val
-            dfs(root.left)
+            if not root: return  # 如果root为空，则说明root的上一节没有子节点
+            dfs(root.right)  # 如果有子节点，先进入右边，即中序遍历的逆序
+            if self.k == 0: return  # 先从右边找，如果右边就满足条件：有第K大节点，那就return
+            self.k -= 1  # 减一，说明离第k大子节点更进一步了。
+            if self.k == 0: self.res = root.val   # self.k == 0时，说明这时的root就是第k大子节点了，返回self.res
+            dfs(root.left)  # 如果右子树没找到，那就找左子树
 
         self.k = k
         dfs(root)
         return self.res
+    
+    
+# 本文解法基于性质：二叉搜索树的中序遍历为递增序列。根据此性质，易得二叉搜索树的 中序遍历倒序 为 递减序列 。因此，求 “二叉搜索树第 k 大的节点” 可转化为求 “此树的中序遍历倒序的第 k 个节点
