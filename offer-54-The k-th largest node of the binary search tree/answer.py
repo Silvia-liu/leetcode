9d8f1ed987eae36925daf9ef8473b52abbbedea8
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-08 13:49:57 +0800

    Create answer.py

---
+++ offer-54-The k-th largest node of the binary search tree/answer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthLargest(self, root: TreeNode, k: int) -> int:
+        def dfs(root):
+            if not root: return
+            dfs(root.right)
+            if self.k == 0: return
+            self.k -= 1
+            if self.k == 0: self.res = root.val
+            dfs(root.left)
+
+        self.k = k
+        dfs(root)
+        return self.res
