abc3970c2414a50ad85fc61804e7441392d39d59
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-06-27 19:51:38 +0800

    Update answer.py

--- offer_55-maximum-depth-of-binary-tree/answer.py
+++ offer_55-maximum-depth-of-binary-tree/answer.py
@@ -3,7 +3,7 @@ class Solution:
     # def maxDepth(self, root: TreeNode) -> int:
     #     if not root:return 0
     #     return max(self.maxDepth(root.left),self.maxDepth(root.right))+1
-    # 广度优先遍历bfs--层序遍历
+    # 广度优先遍历bfs--层序遍历#
     def maxDepth(self,root:TreeNode)->int:
         if not root: return 0
         queue,res = [root], 0        
