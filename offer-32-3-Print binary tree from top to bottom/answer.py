f20f8c835c189dc7c9da16d74c3c19fb81a36c0c
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-07-07 16:25:28 +0800

    Update answer.py

--- offer-32-3-Print binary tree from top to bottom/answer.py
+++ offer-32-3-Print binary tree from top to bottom/answer.py
@@ -24,4 +24,50 @@ class Solution:
 # 打印奇数层： 从左向右 打印，先左后右 加入下层节点；
 # 若 deque 为空，说明向下无偶数层，则跳出；
 # 打印偶数层： 从右向左 打印，先右后左 加入下层节点；
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, deque = [], collections.deque()
+        deque.append(root)
+        while deque:
+            tmp = []
+            # 打印奇数层
+            for _ in range(len(deque)):
+                # 从左向右打印
+                node = deque.popleft()
+                tmp.append(node.val)
+                # 先左后右加入下层节点
+                if node.left: deque.append(node.left)
+                if node.right: deque.append(node.right)
+            res.append(tmp)
+            if not deque: break # 若为空, 说明向下无偶数层,则提前跳出
+            # 打印偶数层
+            tmp = []
+            for _ in range(len(deque)):
+                # 从右向左打印
+                node = deque.pop()
+                tmp.append(node.val)
+                # 先右后左加入下层节点
+                if node.right: deque.appendleft(node.right)
+                if node.left: deque.appendleft(node.left)
+            res.append(tmp)
+        return res
+    
+# 方法三，只用列表，对偶数层直接倒序  偶数层倒序： 若 res 的长度为 奇数 ，说明当前是偶数层，则对 tmp 执行 倒序 操作。
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            tmp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                tmp.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(tmp[::-1] if len(res) % 2 else tmp)
+        return res
+
+
 
