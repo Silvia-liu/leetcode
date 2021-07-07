2bda2a3fbb274dcefb9ff563f5e7a9e65f6f3753
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-07-07 14:05:30 +0800

    Update answer.py

--- offer-32-1-Print binary tree from top to bottom/answer.py
+++ offer-32-1-Print binary tree from top to bottom/answer.py
@@ -1,3 +1,14 @@
+题目要求的二叉树的 从上至下 打印（即按层打印），又称为二叉树的 广度优先搜索（BFS）。BFS 通常借助 队列 的先入先出特性来实现。
+算法流程：
+特例处理： 当树的根节点为空，则直接返回空列表 [] ；
+初始化： 打印结果列表 res = [] ，包含根节点的队列 queue = [root] ；
+BFS 循环： 当队列 queue 为空时跳出；
+      出队： 队首元素出队，记为 node；
+      打印： 将 node.val 添加至列表 tmp 尾部；
+      添加子节点： 若 node 的左（右）子节点不为空，则将左（右）子节点加入队列 queue ；
+返回值： 返回打印结果列表 res 即可。
+
+
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[int]:
         if not root: return []  # 空值判断
