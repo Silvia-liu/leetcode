f2969e868e00d6fba1c2af453baf8755873636e6
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-07-14 10:09:01 +0800

    Update answer.py

--- offer-27-invert-binary-tree/answer.py
+++ offer-27-invert-binary-tree/answer.py
@@ -6,7 +6,3 @@ class Solution:
         root.right = self.mirrorTree(tmp)
         return root
 
-作者：Krahets
-链接：https://leetcode-cn.com/leetbook/read/illustration-of-algorithm/59slxe/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
