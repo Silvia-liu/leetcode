0f935fa8743f2a1e56b06f287b60168f52d51040
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-07-16 15:52:37 +0800

    Update anwer.py

--- offer-49-ugly-number-ii/anwer.py
+++ offer-49-ugly-number-ii/anwer.py
@@ -22,9 +22,9 @@ class Solution:
     def nthUglyNumber(self, n: int) -> int:
         dp, a, b, c = [1] * n, 0, 0, 0
         for i in range(1, n):
-            n2, n3, n5 = dp[a] * 2, dp[b] * 3, dp[c] * 5
-            dp[i] = min(n2, n3, n5)
-            if dp[i] == n2: a += 1
+            n2, n3, n5 = dp[a] * 2, dp[b] * 3, dp[c] * 5  # 这个就是规律
+            dp[i] = min(n2, n3, n5)  # 取最小的丑数
+            if dp[i] == n2: a += 1  # 是他 是他 就是他，然后推出下一个丑数，并且自己+1
             if dp[i] == n3: b += 1
             if dp[i] == n5: c += 1
         return dp[-1]
