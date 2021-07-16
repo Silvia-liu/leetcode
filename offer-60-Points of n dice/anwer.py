c04b6f737df65016ef89a91aa5f735d1406a8856
Silvia <54235270+Silvia-liu@users.noreply.github.com>
2021-07-16 16:45:04 +0800

    Create anwer.py

---
+++ offer-60-Points of n dice/anwer.py
@@ -0,0 +1,13 @@
+# 反正我是不会的
+
+class Solution:
+    def dicesProbability(self, n: int) -> List[float]:
+        dp = [1.0 / 6.0] * 6
+        for i in range(2, n + 1):
+            tmp = [0] * (5 * i + 1)
+            for j in range(len(dp)):
+                for k in range(6):
+                    tmp[j + k] += dp[j] / 6
+            dp = tmp
+        return dp
+
